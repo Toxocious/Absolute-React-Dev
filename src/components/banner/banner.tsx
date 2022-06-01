18f183c6993671e543e28dd29c10b305aacd518c
Jess <toxocious@gmail.com>
2022-05-31 21:50:26 -0700

    Implement 'banner' component

---
+++ src/components/banner/banner.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './banner.scss';
+
+export const Banner_Component = () => {
+  return <header></header>;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { Container_Component as Container } from './components/container';
+import { Banner_Component as Banner } from './components/banner';
 
 import { Test_Page } from './pages/test';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Container>
+      <Banner />
       <Test_Page />
     </Container>
   </React.StrictMode>
