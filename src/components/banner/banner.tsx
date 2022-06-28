c7a4cfed4678b10e3d5a26760dda2ed4ec714420
Jess <toxocious@gmail.com>
2022-06-27 19:59:49 -0700

    Add 'banner' component foundation

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
+import Avatar from '/assets/avatars/default.png';
+
 import './banner.scss';
 
+/**
+ * Needs to display:
+ * - Avatar
+ * - Username
+ * - Roster
+ * - Options Cog
+ * - Logout Button
+ */
 export const Banner_Component = () => {
-  return <header></header>;
+  return (
+    <header>
+      <div className='user-bar'>
+        <div className='user-avatar'>
+          <img src={Avatar} />
+        </div>
+        <div className='user-name'>Toxocious</div>
+        <div className='user-roster'>
+          <img />
+          <img />
+          <img />
+          <img />
+          <img />
+          <img />
+        </div>
+        <div className='user-buttons'>
+          <button>COG</button>
+          <button>LOG</button>
+        </div>
+      </div>
+    </header>
+  );
 };
